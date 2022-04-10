b94bad54cfcd0e12cd8d944d8a680075811ecb55
Jake Kim <jakekim@Jake-Kim.local>
2022-04-09 20:26:50 -0600

    Updated Sell.js

--- routes/Sell.js
+++ routes/Sell.js
@@ -9,14 +9,14 @@ URL: http://localhost:3001/api/sell
 Method: POST
 Input: [{
     "Username”: string,
-    “Offer Value”: double,
+    “OfferValue”: double,
     “Title”: string,
-    “Console Name”: string,
-    “Location”: string,
-    “Publisher Name”: string,
-    “Warehouse”: String
+    “ConsoleName”: string,
+    “PublisherLocation”: string,
+    “PublisherName”: string,
+    “WarehouseLocation”: String
 }]
-Output: “id”:int
+Output: “ID”:int
 */
 
 router.post('/',(req,res)=>{
@@ -25,68 +25,83 @@ router.post('/',(req,res)=>{
 	db.query('SELECT ID FROM Video_Game AS V WHERE V.Title=? AND V.ConsoleName=? AND V.PublisherName=?',[req.body.Title,req.body.ConsoleName,req.body.PublisherName],(err1,data1)=>{
 		if (err1) {
 			res.send(400)
+			return
 		}
 		if (data1.length == 0) {
-			newGame = true
-			db.query('INSERT INTO Video_Game (Price,Title,ESRB_Rating,Description,PublisherName,ConsoleName,Admin_ID,IMG_URL) VALUES(?,?,?,?,?,?,?,?)',
-			[null, req.body.Title, null, null, req.body.PublisherName, req.body.ConsoleName, null, null], (err2, data2)=>{
-				if (err2) {
-					res.send(400)
-				}
-				db.query('SELECT LAST_INSERT_ID() AS id',(err4,data4)=>{
-					newGameID = data4[0].id
-				})
-			})
+			// newGame = true
+			// db.query('INSERT INTO Video_Game (Price,Title,ESRB_Rating,Description,PublisherName,ConsoleName,Admin_ID,IMG_URL) VALUES(?,?,?,?,?,?,?,?)',
+			// [null, req.body.Title, null, null, req.body.PublisherName, req.body.ConsoleName, null, null], (err2, data2)=>{
+			// 	if (err2) {
+			// 		res.send(400)
+			// 	}
+			// 	db.query('SELECT LAST_INSERT_ID() AS id',(err4,data4)=>{
+			// 		newGameID = data4[0].id
+			// 	})
+			// })
+			res.send({ID: null})
+			return
 		}
 
-		if (newGame) {
-			db.query('INSERT INTO Makes_Offers (Username, ID, Location, OfferValue) VALUES(?,?,?,?)',
-			[req.body.Username, newGameID, req.body.Warehouse, req.body.OfferValue], (err3, data3)=>{
-				if (err3) {
-					res.send(400)
-				}
-				else {
-					res.send({ID: newGameID})
-				}	
-			})
-		}
-		
-		else {
-			db.query('INSERT INTO Makes_Offers (Username, ID, Location, OfferValue) VALUES(?,?,?,?)',
-			[req.body.Username, data1[0].ID, req.body.Warehouse, req.body.OfferValue], (err5, data5)=>{
+		db.query('INSERT INTO Makes_Offers (Username, ID, Location, OfferValue) VALUES(?,?,?,?)',
+			[req.body.Username, data1[0].ID, req.body.WarehouseLocation, req.body.OfferValue], (err5, data5)=>{
 				if (err5) {
 					res.send(400)
+					return
 				}
 				else {
 					res.send({ID: data1[0].ID})
 				}
 			})
-		}
+
+		// if (newGame) {
+		// 	db.query('INSERT INTO Makes_Offers (Username, ID, Location, OfferValue) VALUES(?,?,?,?)',
+		// 	[req.body.Username, newGameID, req.body.Warehouse, req.body.OfferValue], (err3, data3)=>{
+		// 		if (err3) {
+		// 			res.send(400)
+		// 		}
+		// 		else {
+		// 			res.send({ID: newGameID})
+		// 		}	
+		// 	})
+		// }
+		
+		// else {
+		// 	db.query('INSERT INTO Makes_Offers (Username, ID, Location, OfferValue) VALUES(?,?,?,?)',
+		// 	[req.body.Username, data1[0].ID, req.body.Warehouse, req.body.OfferValue], (err5, data5)=>{
+		// 		if (err5) {
+		// 			res.send(400)
+		// 		}
+		// 		else {
+		// 			res.send({ID: data1[0].ID})
+		// 		}
+		// 	})
+		// }
 	})
 })
 
 /*
 Endpoint 16: Making offers (Displaying previous offers they’ve made)
 Description: Get previous offers
-URL: https://localhost:3001/api/sell/{user}
+URL: https://localhost:3001/api/sell/{Username}
 Method: GET
 Input: [{
     "Username”: string
 }]
 Output [{
     “offers” : [{
-		“g_id” : int
-		“offer_val” : double
-		“status” : string
+		“ID” : int
+		“OfferValue” : double
+		“Status” : string
 }]
 }]
 */
 
-router.get(('/:user', (req,res)=>{
-	const { user } = req.params
-	db.query('SELECT ID, OfferValue, Status FROM Video_Game WHERE Username=?',[user],(err,data)=>{
+router.get(('/:Username', (req,res)=>{
+	const { Username } = req.params
+	db.query('SELECT ID, OfferValue, Status FROM Video_Game WHERE Username=?',[Username],(err,data)=>{
 		if (err) {
-			res.send(400)
+			throw err
+			return
 		}
 		else {
 			res.send(data)
@@ -97,7 +112,7 @@ router.get(('/:user', (req,res)=>{
 /*
 Endpoint 17: Get Offers
 Description: Admin view for getting offers from a warehouses they manage
-URL: https://localhost:3001/api/sell/{user}
+URL: https://localhost:3001/api/sell/admin/{Admin_ID}
 Method: GET
 Input: [{
     “Admin_id”: int
@@ -113,10 +128,11 @@ Output: [{
 }]
 */
 
-router.get(('/:user', (req,res)=>{
+router.get(('/admin/:Admin_ID', (req,res)=>{
 	db.query('SELECT * FROM Admin AS A, Makes_Offers AS M, Manages AS K WHERE Admin.Admin_ID=? AND A.Admin_ID=K.Admin_ID AND M.Location=K.Location',[req.body.Admin_ID],(err,data)=>{
 		if (err) {
 			res.send(400)
+			return
 		}
 		else {
 			res.send(data)
@@ -143,6 +159,7 @@ router.post('/:user/decision',(req,res)=>{
 		db.query('SELECT Location FROM Makes_Offers AS M WHERE M.Username=? AND M.ID=? AND M.Status=?',[req.params.Username, req.body.g_id, "Pending"],(err,data)=>{
 			if (err) {
 				res.send(400)
+				return
 			}
 			else {
 				offerLocation = data[0].Location
@@ -151,12 +168,14 @@ router.post('/:user/decision',(req,res)=>{
 		db.query('UPDATE Makes_Offers SET Makes_Offers.Status="Approved" WHERE Makes_Offers.Username=? AND Makes_Offers.ID=? AND Makes_Offers.Status=?',[req.params.Username, req.body.g_id, "Pending"],(err2, data2)=>{
 			if (err2) {
 				res.send(400)
+				return
 			}	
 		})
 
 		db.query('UPDATE Has_Stock SET Has_Stock.Quantity=Has_Stock.Quantity+1 WHERE Has_Stock.Location=?',[offerLocation],(err3,data3)=>{
 			if (err3) {
 				res.send(400)
+				return
 			}
 			else {
 				res.send({status: true})
@@ -168,6 +187,7 @@ router.post('/:user/decision',(req,res)=>{
 		db.query('UPDATE Makes_Offers SET Makes_Offers.Status="Declined" WHERE Makes_Offers.Username=? AND Makes_Offers.ID=? AND Makes_Offers.Status=?',[req.params.Username, req.body.g_id, "Pending"],(err4, data4)=>{
 			if (err4) {
 				res.send(400)
+				return
 			}
 			else {
 				res.send({status: false})
